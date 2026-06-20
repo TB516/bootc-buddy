f770c1e6ab92504d0eda7da40800117657c64cf8
Thomas <77514919+TB516@users.noreply.github.com>
2026-06-20 18:44:24 -0400

    restructured project for flatpak first dev

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+export default {
+  server: {
+    watch: {
+      ignored: [
+        "**/.flatpak-build/**",
+        "**/.flatpak-build-release/**",
+        "**/.flatpak-builder*/**",
+      ],
+    },
+  },
+};
